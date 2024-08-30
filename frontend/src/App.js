e9f56f0155cd63617ee0178b84a293a471238a2c
Deepak Jhawar <deepakjhawar04@gmail.com>
2024-08-30 09:51:11 +0530

    setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 className='text-5xl text-white bg-black'>Hello</h1>
     </div>
   );
 }
