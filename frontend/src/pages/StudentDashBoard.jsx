124c85414ce16bebc2c39770f27ee53f8fd53a57
Yojith Kaustabh <yojith212@gmail.com>
2024-09-21 11:08:39 +0530

    .

--- frontend/src/pages/StudentDashBoard.jsx
+++ frontend/src/pages/StudentDashBoard.jsx
@@ -1,17 +1,28 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import Sidebar from '../components/Sidebar'
-import AttendanceChart from '../components/AttendanceChart'
+import { Box, Grid } from '@mui/material';
+import React from 'react';
+import Sidebar from '../components/Sidebar';
+import AttendanceChart from '../components/AttendanceChart';
+import TicketTable from '../components/TicketTable';
 
 function StudentDashboard() {
   return (
-    <Box>
-      {/* <Sidebar /> */}
-      <div>
-        <AttendanceChart />
-      </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        {/* Sidebar takes 2 out of 12 grid spaces */}
+        <Grid item xs={12} md={2}>
+          <Sidebar />
+        </Grid>
+
+        {/* Main content takes the remaining 10 out of 12 grid spaces */}
+        <Grid item xs={12} md={10}>
+          <Box>
+            <AttendanceChart />
+            <TicketTable />
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
-  )
+  );
 }
 
-export default StudentDashboard
+export default StudentDashboard;
