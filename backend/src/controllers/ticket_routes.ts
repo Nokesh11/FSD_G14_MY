3e6b4e7a10404e356a8476b48da3ab55926539ce
Deepak Jhawar <deepakjhawar04@gmail.com>
2024-09-27 08:40:22 +0530

    .

--- backend/src/controllers/ticket_routes.ts
+++ backend/src/controllers/ticket_routes.ts
@@ -1,26 +1,32 @@
 import { Router } from 'express';
 import { admin_type, debugEnum, powerType } from '../shared';
 import { Central } from '../models/central_db';
-import { Admin, Condition, ObjectId } from 'mongodb';
+import { Admin, Condition, ObjectId, PushOperator } from 'mongodb';
 import { TicketDB } from '../models/tickets_db';
 
 export const app = Router();
 
 app.post('/create-ticket', async(req, res) =>
 {
-    if (req.session!.authorized === true )
+    if (req.session!.authenticated === true )
     {
         const instID = req.session!.instID;
-        const usereID = req.session!.instID;
-        const type = req.session!.instID;
+        const userID = req.session!.userID;
+        const type = req.session!.type;
         const { body, title, toID } = req.body;
         const result = await Central.getUser(toID, admin_type, instID);
         if (result.message !== debugEnum.SUCCESS)
         {
             return res.status(401).send({message : "ToID is not an admin or does not exit"})
         }
-        const col = result.col;
-        await col!.insertOne({_id : TicketDB.genTicketID(usereID, toID),body : body, title : title } as Condition<ObjectId>);
+
+        const ticket_col = result.instDB?.ticket_col;
+        const ticketID = TicketDB.genTicketID(userID, toID);
+        await ticket_col!.insertOne({_id : ticketID ,body : body, title : title } as Condition<ObjectId>);
+
+        
+        const Colresult = await Central.getCol(instID, type);
+        Colresult.col!.updateOne({_id : userID},{$push : {active_tickets : ticketID}} as PushOperator<Document>);;
         return res.status(200).send({message : "Ticket created successfully"});
     }
     return res.status(401).send({message : "Unauthorized"});
