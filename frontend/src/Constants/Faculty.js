bfb683013cedacac21fac8d04c16fb1b8d8e9f68
Yojith Kaustabh <yojith212@gmail.com>
2024-09-27 06:03:15 +0530

    added faculty constants

---
+++ frontend/src/Constants/Faculty.js
@@ -0,0 +1,11 @@
+import { Home, Calendar, FileText, Clock, Book, Percent, TicketMinus } from 'lucide-react';
+
+export const student = [
+    { icon: <Home />, text: "Dashboard", path: "dashboard" },
+    // { icon: <Calendar />, text: "Attendance", path: "attendance" },
+    { icon: <TicketMinus />, text: "Pending Ticket", path: "resolve_tickets" },
+    { icon: <FileText />, text: "DocVault", path: "docvault" },
+    { icon: <Clock />, text: "TimeTable", path: "timetable" },
+    { icon: <Book />, text: "Almanac", path: "almanac" },
+    { icon: <Percent />, text: "Scores", path: "scores" },
+];
